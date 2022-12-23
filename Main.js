a087a9c15e03f0f1b46b40a9bf773319c231d756
Not-Jimbo <103302869+Not-Jimbo@users.noreply.github.com>
2022-12-23 17:07:19 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,1 +1,25 @@
+function getKeyString(x, y) {
+    return '${x}x${y}';
+}
 
+function (){
+    
+    let playerId;
+    let playerRef;
+    
+    firebase.auth().onAuthStateChanged(user) => {
+        if(
+        playerId = user.uid;
+        playerRef = firebase.database().ref('players/${playerId}');
+        
+        playerRef.set({
+            id: playerId,
+            name: "",
+            x: 10,
+            y: 10,
+        })
+    })
+    
+    firebase.auth().signInAnonymously()
+
+})();
